c13273b8017c47f3c6ad389bba211a0fbfb2c460
Paweł J <jpawz@outlook.com>
2022-05-09 18:20:49 +0200

    Add tests

--- lcw-ui/src/app/export.service.spec.ts
+++ lcw-ui/src/app/export.service.spec.ts
@@ -1,16 +1,53 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ExportService } from './export.service';
+import { Word } from './word';
 
 describe('ExportService', () => {
   let service: ExportService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
     service = TestBed.inject(ExportService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('#exportCSV should response with csv', (done) => {
+    const wordsToSubmit: Word[] = [{ id: 0, rank: 0, sentence: '', translation: 'the translation', word: 'word' }];
+    const expectedCsv = new Blob(['word,the translation,,'], { type: 'text/csv' });
+
+    service.exportCSV(wordsToSubmit).subscribe(async data => {
+      const text = await (new Response(data)).text();
+      expect(text).toEqual('word,the translation,,');
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne('http://localhost:8080/export/csv');
+
+    testRequest.flush(expectedCsv);
   });
+
+  it('#exportCSV should use POST to retrieve csv data', () => {
+    const wordsToSubmit: Word[] = [{ id: 0, rank: 0, sentence: '', translation: 'the translation', word: 'word' }];
+    service.exportCSV(wordsToSubmit).subscribe();
+
+    const testRequest = httpTestingController.expectOne('http://localhost:8080/export/csv');
+
+    expect(testRequest.request.method).toEqual('POST');
+  });
+
+  it('#exportCSV POST request should contain correct body', () => {
+    const wordsToSubmit: Word[] = [{ id: 0, rank: 0, sentence: '', translation: 'the translation', word: 'word' }];
+    service.exportCSV(wordsToSubmit).subscribe();
+
+    const testRequest = httpTestingController.expectOne('http://localhost:8080/export/csv');
+
+    expect(testRequest.request.body).toContain({ word: 'word', translation: 'the translation', sentence: '' });
+  });
+
 });
