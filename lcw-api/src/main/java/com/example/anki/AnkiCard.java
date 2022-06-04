86758ad5d5f62663e0074b23ad5ce2e6e019b42d
Paweł J <jpawz@outlook.com>
2022-06-04 10:40:08 +0200

    Switched class to record

--- lcw-api/src/main/java/com/example/anki/AnkiCard.java
+++ lcw-api/src/main/java/com/example/anki/AnkiCard.java
@@ -2,30 +2,11 @@ package com.example.anki;
 
 import java.util.Map;
 
-public class AnkiCard {
-  private final Map<String, String> question;
-  private final Map<String, String> answer;
+/**
+ * 
+ * Class represents an AnkiCard: question and answer fields.
+ *
+ */
+public record AnkiCard(Map<String, String> question, Map<String, String> answer) {
 
-  /**
-   * Add fields to AnkiCard. 
-   * @param question Map of "field_name" and "value" for question part of card.
-   * @param answer Map of "field_name" and "value" for answer part of card.
-   */
-  public AnkiCard(Map<String, String> question, Map<String, String> answer) {
-    this.question = question;
-    this.answer = answer;
-  }
-
-  public Map<String, String> getQuestion() {
-    return question;
-  }
-
-  public Map<String, String> getAnswer() {
-    return answer;
-  }
-
-  @Override
-  public String toString() {
-    return "Q:" + question.toString() + " A:" + answer.toString();
-  }
 }
