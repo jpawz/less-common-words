a1554c61db8b13215b4e128736014241d7cf5416
Paweł J <jpawz@outlook.com>
2022-06-21 19:36:31 +0200

    Add optional parameter to WordRankController method Added optional "limit" parameter to getWordRanks method

--- lcw-api/src/main/java/com/example/controller/WordRankController.java
+++ lcw-api/src/main/java/com/example/controller/WordRankController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,21 +21,25 @@ import com.example.service.WordsRankService;
 @RequestMapping("api")
 public class WordRankController {
 
-	@Autowired
-	private WordsRankService service;
+    @Autowired
+    private WordsRankService service;
 
-	@GetMapping
-	public WordRank getWordRank(@RequestParam String word) {
-		return service.getWordRankForWord(word);
-	}
+    @GetMapping
+    public WordRank getWordRank(@RequestParam String word) {
+	return service.getWordRankForWord(word);
+    }
 
-	@PostMapping
-	public WordRank saveWordRank(@RequestParam String word, @RequestParam int rank) {
-		return service.saveWordsRank(new WordRank(word, rank));
-	}
+    @PostMapping
+    public WordRank saveWordRank(@RequestParam String word, @RequestParam int rank) {
+	return service.saveWordsRank(new WordRank(word, rank));
+    }
 
-	@PostMapping("words")
-	public List<WordRank> getWordRanks(@RequestBody Set<String> words) {
-		return service.getWordsRankForWords(words);
-	}
+    @PostMapping("words")
+    public List<WordRank> getWordRanks(@RequestBody Set<String> words, @RequestParam Optional<Integer> limit) {
+	if (limit.isEmpty())
+	    return service.getWordsRankForWords(words);
+	else
+	    return service.getWordsRankForWordsWhereRankIsGreaterThan(words, limit.get());
+    }
+
 }
--- lcw-api/src/test/java/com/example/controller/WordRankControllerTest.java
+++ lcw-api/src/test/java/com/example/controller/WordRankControllerTest.java
@@ -2,6 +2,10 @@ package com.example.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -10,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
 import com.example.domain.WordRank;
 
@@ -17,37 +22,51 @@ import com.example.domain.WordRank;
 @TestInstance(Lifecycle.PER_CLASS)
 class WordRankControllerTest {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	private String url;
+    private String url;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@BeforeEach
-	public void setup() {
-		url = String.format("http://localhost:%d/api", port);
-	}
+    @BeforeEach
+    public void setup() {
+	url = String.format("http://localhost:%d/api", port);
+    }
 
-	@Test
-	void whenGetRequestForExistingWord_thenReturnRank() {
-		String existingWord = "and";
+    @Test
+    void whenGetRequestForExistingWord_thenReturnRank() {
+	String existingWord = "and";
 
-		WordRank wordRank = this.restTemplate.getForObject(url + "?word=" + existingWord, WordRank.class);
+	WordRank wordRank = this.restTemplate.getForObject(url + "?word=" + existingWord, WordRank.class);
 
-		assertThat(wordRank.getRank()).isEqualTo(3);
-	}
+	assertThat(wordRank.getRank()).isEqualTo(3);
+    }
 
-	@Test
-	void whenPostNewWord_thenReturnWordRank() {
-		String newWord = "undercarriage";
-		int newRank = 99999;
+    @Test
+    void whenPostNewWord_thenReturnWordRank() {
+	String newWord = "undercarriage";
+	int newRank = 99999;
 
-		WordRank wordRank = this.restTemplate.postForObject(url + "?word=" + newWord + "&rank=" + newRank,
-				new WordRank(newWord, newRank), WordRank.class);
+	WordRank wordRank = this.restTemplate.postForObject(url + "?word=" + newWord + "&rank=" + newRank,
+		new WordRank(newWord, newRank), WordRank.class);
 
-		assertThat(wordRank.getRank()).isEqualTo(newRank);
-	}
+	assertThat(wordRank.getRank()).isEqualTo(newRank);
+    }
+
+    @Test
+    void whenPostRequestForWordsWithLimit_thenReturnRanks() {
+	int limit = 40;
+	String rank27 = "new";
+	String rank41 = "search";
+	String rank42 = "free";
+	Set<String> words = new HashSet<>(Arrays.asList(rank27, rank41, rank42));
+
+	ResponseEntity<WordRank[]> response = restTemplate.postForEntity(url + "/words?limit=" + limit, words,
+		WordRank[].class);
+
+	assertThat(response.getBody()).containsExactlyInAnyOrder(new WordRank(rank41, 41), new WordRank(rank42, 42));
+    }
 
 }
